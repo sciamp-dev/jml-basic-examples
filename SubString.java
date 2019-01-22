bf0286aa44b9b1e8af96a6481abda75ca123d966
Dennis Campagna <denniscampagna@gmail.com>
2019-01-22 14:50:28 +0100

    SubString exercise

---
+++ SubString.java
@@ -0,0 +1,21 @@
+/*
+    Write a specific in JML that gives sense to the method, given the following example:
+    - text = [abbcdddeff], chunk = [bcddd] -> returns true
+    - text = [abbcdddeff], chunk = [baadd] -> returns false
+*/
+
+public class SubString {
+    // Since chunk is a substring of text, we can say that chunk.legth must be equal or lower then text.length
+    // We clearly don't want null parameters
+    // The method returns true only if chunk is sub string of text
+    
+    //@ requires text != null
+    //@     && chunk != null
+    //@     && chunk.length <= text.length;
+    //@ ensures (\exist int i; i >= 0 && i < text.length; 
+    //@             (\forall int j = 0; j >= 0 && j < chunk.length; text[j+i] == chunk[j])
+    //@         ) <==> \result == true;
+    public static boolean subString(char[] text, char[] chunk) {
+        // ...
+    }
+}
--- TimeInterval.java
+++ TimeInterval.java
@@ -3,7 +3,7 @@
     - times not null
     - times strictly increasing
     - return an object of type TimeInterval with a temporal range having as two extremes contiguous points of times
-    - timePoint must be equal or greater of the minor end and strictly lower of the major end
+    - timePoint must be equal or greater then the minor end and strictly lower then the major end
 */
 
 public class TimeInterval {
@@ -18,5 +18,12 @@ public class TimeInterval {
         //...
     }
 
-    public static TimeInterval getInterval(float[] times, float timePoint) {}
+    //@ requires times != null
+    //@     && (\forall int i = 0; i >= 0 && i < (times.length - 1); times[i] < times[i+1]);
+    //@ ensures (\exist int j; j >=0 && j < (times.length - 1); (times[j] ==  \result.getLow() && times[j+1] == \result.getHigh))
+    //@     && timePoint >= \result.getLow()
+    //@     && timePoint < \result.getHigh();
+    public static TimeInterval getInterval(float[] times, float timePoint) {
+        // ...
+    }
 }
